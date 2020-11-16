989f2615b42a64b876456714229eea6236be9869
TsarkFC <up201806779@fe.up.pt>
2020-11-16 15:42:15 +0000

    Moving pieces correctly

--- src/game.pl
+++ src/game.pl
@@ -7,21 +7,58 @@ initial(game_state(white, npieces(8, 8), Board)) :- board(Board).
 
 
 % toggle_player(+CurrentPlayer, -NextPlayer)
-toggle_player(game_state(white, NPieces, Board), game_state(black, NPieces, Board)).
-toggle_player(game_state(black, NPieces, Board), game_state(white, NPieces, Board)).
+toggle_player(white, black).
+toggle_player(black, white).
 toggle_player(P, _) :- 
 	print('Invalid current player: '),
 	print(P),
 	nl,
 	fail.
 
+updateBoardColumn(OldH, NewH, move(position(X1, Y1), position(X2, Y2), Piece), ColumnNumber, LineNumber) :-
+	(X1 =:= ColumnNumber, Y1 =:= LineNumber -> NewH = empty;
+		(X2 =:= ColumnNumber, Y2 =:= LineNumber
+			-> NewH = Piece
+			; NewH = OldH
+		)
+	).
+
+iterateBoardColumn([OldH|[]], [NewH|_], Move, ColumnNumber, LineNumber) :-
+	updateBoardColumn(OldH, NewH, Move, ColumnNumber, LineNumber).
+
+iterateBoardColumn([OldH|OldT], [NewH|NewT], Move, ColumnNumber, LineNumber) :-
+	updateBoardColumn(OldH, NewH, Move, ColumnNumber, LineNumber),
+	NextColumnNumber is ColumnNumber + 1,
+	iterateBoardColumn(OldT, NewT, Move, NextColumnNumber, LineNumber).
+
+
+updateBoardLine(OldH, NewH, move(position(X1, Y1), position(X2, Y2), Piece), RowNumber) :-
+	(
+		Y1 \= RowNumber, Y2 \= RowNumber 
+			-> NewH = OldH
+			; iterateBoardColumn(OldH, NewH, move(position(X1, Y1), position(X2, Y2), Piece), 1, RowNumber)
+	).
+
+iterateBoardLine([OldH|[]], [NewH|_], Move, RowNumber) :-
+	updateBoardLine(OldH, NewH, Move, RowNumber).
+
+iterateBoardLine([OldH|OldT], [NewH|NewT], Move, RowNumber) :-
+	updateBoardLine(OldH, NewH, Move, RowNumber),
+	NextRowNumber is RowNumber + 1,
+	iterateBoardLine(OldT, NewT, Move, NextRowNumber).
+
+
+move_piece(Move, CurrentBoard, NextBoard) :-
+	iterateBoardLine(CurrentBoard, NextBoard, Move, 1).
+
 
 % Applies a move
-% apply_move(+Move, +CurrentGameState, -NextGameState)
-apply_move(move(position(X1, Y1), position(X2, Y2), Piece), CurrentGameState, NextGameState) :-
-	%find_piece(position(X1, Y1), Piece),
-	%place_piece(position(X2, Y2), Piece)
-	toggle_player(CurrentGameState, NextGameState).
+% apply_move(+Move, +CurrentGameState, -NextGameState) 
+apply_move(Move, game_state(CurrentPlayer, CurrentNPieces, CurrentBoard), game_state(NextPlayer, CurrentNPieces, NextBoard)) :-
+	%find_piece(position(X1, Y1), Piece, CurrentGameState, IntermediateGameState),
+	%place_piece(position(X2, Y2), Piece, IntermediateGameState, NextGameState),
+	move_piece(Move, CurrentBoard, NextBoard),
+	toggle_player(CurrentPlayer, NextPlayer).
 
 get_desired_position_input(position(X1, Y1), GameBoard, DesiredOccupant):-
 	get_position_input(position(XTemp, YTemp)),
@@ -40,7 +77,7 @@ get_move(move(Position1, Position2, Piece), game_state(CurrentPlayer, _, GameBoa
 	print('Desired place:\n'),
 	get_desired_position_input(position(X2, Y2), GameBoard, empty),
 
-	Position1 = position(X1, Y1), Position2 = position(X2, Y2).
+	Position1 = position(X1, Y1), Position2 = position(X2, Y2), Piece = CurrentPlayer.
 
 % The C arg is just a counter to simulate the game end (Ends when C == 3)
 % In reality, the game ends when one of the players only has one piece, and the winner is the other
