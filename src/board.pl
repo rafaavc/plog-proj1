2b1050bec598a81f22c1afd5e6a7dcf48f46f661
TsarkFC <up201806779@fe.up.pt>
2020-11-30 00:56:57 +0000

    Three dragons v1.0

--- src/board.pl
+++ src/board.pl
@@ -1,6 +1,5 @@
-%database
-
 % board(-Board)
+% Unifies 'Board' with the initial board
 board([
 	[mountain, dice(black, 3), dice(black, 2), dice(black, 2), dice(black, 2), dice(black, 2), dice(black, 2), dice(black, 3), mountain],
 	[empty, empty, empty, empty, dice(black, 4), empty, empty, empty, empty],
@@ -13,40 +12,110 @@ board([
 	[mountain, dice(white, 3), dice(white, 2), dice(white, 2), dice(white, 2), dice(white, 2), dice(white, 2), dice(white, 3), mountain]
 ]).
 
-/*
-% intermediate state
-board([
-	[6, 7],
-	[mountain, empty, black, empty, black, empty, black, empty, mountain],
-	[empty, empty, empty, empty, black, empty, empty, empty, white],
-	[empty, empty, empty, empty, empty, empty, white, empty, empty],
-	[empty, empty, empty, empty, black, empty, empty, empty, empty],
-	[dragonCave, empty, empty, empty, dragonCave, empty, empty, empty, dragonCave],
-	[white, empty, empty, black, empty, empty, empty, empty, empty],
-	[empty, empty, empty, empty, empty, empty, empty, empty, black],
-	[empty, empty, empty, empty, white, empty, empty, empty, empty],
-	[mountain, empty, white, empty, empty, white, empty, empty, mountain]
-]).
-*/
 
-/*
-% final state
-board([
-	[1, 5],
-	[mountain, empty, black, empty, empty, empty, black, empty, mountain],
-	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
-	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
-	[empty, empty, empty, empty, black, empty, empty, empty, empty],
-	[dragonCave, empty, empty, empty, dragonCave, empty, empty, empty, dragonCave],
-	[empty, empty, empty, black, empty, empty, empty, empty, empty],
-	[empty, empty, empty, empty, empty, empty, empty, empty, black],
-	[empty, empty, empty, empty, empty, empty, empty, empty, empty],
-	[mountain, empty, white, empty, empty, empty, empty, empty, mountain]
-]).
-*/
+% cave_position(+Position)
+% Succeeds if 'Position' corresponds to a dragon cave.
+% Else fails
+cave_position(Position) :-
+	Position = position(1, 5);
+	Position = position(5, 5);
+	Position = position(9, 5).
 
-% establishes correspondence between each atom and the symbol to be displayed
+
+% update_board_column(+Index, +NewElement, +Row, -NewRow)
+% Unifies 'NewRow' with the updated version of 'Row', after updating the row with index 'Index' to the 'NewElement'
+update_board_column(Index, NewElement, Row, NewRow) :-
+	nth1(Index, Row, _Element, Rest),
+	nth1(Index, NewRow, NewElement, Rest).
+
+% update_board_row(+Index, +NewRow, +Board, -NewBoard)
+% Unifies 'NewBoard' with the updated version of 'Board', after updating the row with index 'Index' to the 'NewRow'
+update_board_row(Index, NewRow, Board, NewBoard) :-
+	nth1(Index, Board, _Row, Rest),
+	nth1(Index, NewBoard, NewRow, Rest).
+
+% add_pieces(+PiecePositions, +GameBoard, -NextGameBoard)
+% Unifies 'NextBoard' with the updated version of the 'CurrentBoard', after adding the new 'Pieces'
+add_pieces([], NextBoard, NextBoard).
+add_pieces([piecePosition(position(X, Y), Piece)|PiecesT], CurrentBoard, TempBoard) :-
+	nth1(Y, CurrentBoard, Row),
+	update_board_column(X, Piece, Row, NewRow),
+	update_board_row(Y, NewRow, CurrentBoard, TempBoard),
+	add_pieces(PiecesT, TempBoard, TempBoard).
+
+% remove_pieces(+Pieces, +CurrentBoard, -NextGameBoard)
+% Unifies 'NextBoard' with the updated version of the 'CurrentBoard', after removing the 'Pieces'
+remove_pieces([], NextBoard, NextBoard).
+remove_pieces([piecePosition(position(X, Y), _Piece)|PiecesT], CurrentBoard, NextBoard) :-
+	nth1(Y, CurrentBoard, Row),
+	(cave_position(position(X, Y)) -> 
+	update_board_column(X, dragonCave(invoked), Row, NewRow) ;
+	update_board_column(X, empty, Row, NewRow)),
+	update_board_row(Y, NewRow, CurrentBoard, TempBoard),
+	remove_pieces(PiecesT, TempBoard, NextBoard).
+
+
+% nth_board_element(+Board, +Position, -Element)
+% Unifies 'Element' with the element that's in the given 'Position' of the 'Board'
+nth_board_element(Board, position(X, Y), Element) :-
+	nth1(Y, Board, Line),
+	nth1(X, Line, Element).
+
+
+% get_dice(+Dice, -Piece)
+% Gets piece value from dice
+get_dice(dice(Piece, Strength), Piece, Strength).
+
+% get_value_from_dice(+Piece, -Value)
+% Gets piece value from Piece either it is represented by a dice or not
+get_value_from_dice(Piece, Value, Strength) :-
+	(
+		(
+			Piece = empty ; Piece = dragonCave(empty) ; Piece = dragonCave(invoked) ; Piece = mountain
+		) -> (Value = Piece, Strength = 0);
+		get_dice(Piece, Value, Strength)
+	).
+
+% get_opposite_type(+Piece, -OppositePiece)
+% Gets the opposite piece
+get_opposite_type(black, white).
+get_opposite_type(white, black).
+
+
+% get_pieces_orthogonal_to_position(+Position, +GameBoard, -L)
+% Unifies 'L' with the list of the (most commonly) 4 pieces that are orthogonally placed next to the 'Position' of the current 'GameBoard'
+get_pieces_orthogonal_to_position(Position, GameBoard, L) :-
+    (get_piece_with_xy_offset(Position, 1, 0, GameBoard, piecePosition(_PosRight, RightOfPiece)) ;true),
+	(get_piece_with_xy_offset(Position, -1, 0, GameBoard, piecePosition(_PosLeft, LeftOfPiece)) ;true),
+	(get_piece_with_xy_offset(Position, 0, 1, GameBoard, piecePosition(_PosBottom, BottomOfPiece)) ;true),
+	(get_piece_with_xy_offset(Position, 0, -1, GameBoard, piecePosition(_PosTop, TopOfPiece)) ;true),
+    
+	build_valid_list([RightOfPiece, LeftOfPiece, BottomOfPiece, TopOfPiece], [], L).
+
+% get_pieces_with_offset_orthogonal_to_position(+Position, +GameBoard, -L)
+% Unifies 'L' with the list of the (most commonly) 4 pieces, with their offsets from 'Position', that are orthogonally placed next to the 'Position' of the current 'GameBoard'
+get_pieces_with_offset_orthogonal_to_position(Position, GameBoard, L) :-
+    (get_piece_with_xy_offset(Position, 1, 0, GameBoard, piecePosition(_PosRight, RightOfPiece)) ;true),
+	(get_piece_with_xy_offset(Position, -1, 0, GameBoard, piecePosition(_PosLeft, LeftOfPiece)) ;true),
+	(get_piece_with_xy_offset(Position, 0, 1, GameBoard, piecePosition(_PosBottom, BottomOfPiece)) ;true),
+	(get_piece_with_xy_offset(Position, 0, -1, GameBoard, piecePosition(_PosTop, TopOfPiece)) ;true),
+    
+	build_valid_list([pieceXYOffset(xyOffset(1, 0), RightOfPiece), pieceXYOffset(xyOffset(-1, 0), LeftOfPiece), pieceXYOffset(xyOffset(0, 1), BottomOfPiece), pieceXYOffset(xyOffset(0, -1), TopOfPiece)], [], L).
+
+% get_piece_with_xy_offset(+PiecePosition, +Xvar, +Yvar, +GameBoard, -Piece)
+% Gets piece in position that corresponds to the sum of the current PiecePosition coordinates with the given offset: Xvar, Yvar
+get_piece_with_xy_offset(position(X, Y), Xvar, Yvar, GameBoard, piecePosition(position(X1, Y1), Piece)) :-
+	Y1 is Y+Yvar,
+	Y1 > 0,
+	Y1 =< 9,
+	nth1(Y1, GameBoard, Line),
+	X1 is X+Xvar,
+	X1 > 0,
+	X1 =< 9,
+	nth1(X1, Line, Piece).
+
 % symbol(+Atom, -Symbol)
+% establishes correspondence between each atom and the symbol to be displayed
 symbol(mountain, 'M ').
 symbol(empty, '  ').
 
@@ -61,30 +130,37 @@ symbol(dice(white, Value), Sym) :-
 symbol(dragonCave(empty), 'D ').
 symbol(dragonCave(invoked), 'DI').
 
-%displays the board's grid intermediate elements
+
+% display_board_middle_separator
+% displays the board's middle horizontal line separator
 display_board_middle_separator :-
 	print('  \x251C\\x2500\\x2500\\x2500\\x2500\\x253C\\x2500\\x2500\\x2500\\x2500\\x253C\\x2500\\x2500\\x2500\\x2500\\x253C\\x2500\\x2500\\x2500\\x2500\\x253C\\x2500\\x2500\\x2500\\x2500\\x253C\\x2500\\x2500\\x2500\\x2500\\x253C\\x2500\\x2500\\x2500\\x2500\\x253C\\x2500\\x2500\\x2500\\x2500\\x253C\\x2500\\x2500\\x2500\\x2500\\x2524\\n').
 
-%displays the board's grid top line
+% display_board_top_separator
+% displays the board's top horizontal border separator
 display_board_top_separator :-
 	print('    A    B    C    D    E    F    G    H    I\n'),
 	print('  \x250C\\x2500\\x2500\\x2500\\x2500\\x252C\\x2500\\x2500\\x2500\\x2500\\x252C\\x2500\\x2500\\x2500\\x2500\\x252C\\x2500\\x2500\\x2500\\x2500\\x252C\\x2500\\x2500\\x2500\\x2500\\x252C\\x2500\\x2500\\x2500\\x2500\\x252C\\x2500\\x2500\\x2500\\x2500\\x252C\\x2500\\x2500\\x2500\\x2500\\x252C\\x2500\\x2500\\x2500\\x2500\\x2510\ '),
 	nl.	
 
-%displays the board's grid bottom line
+% display_board_bottom_separator
+% displays the board's ottom horizontal border separator
 display_board_bottom_separator :-
 	print('  \x2514\\x2500\\x2500\\x2500\\x2500\\x2534\\x2500\\x2500\\x2500\\x2500\\x2534\\x2500\\x2500\\x2500\\x2500\\x2534\\x2500\\x2500\\x2500\\x2500\\x2534\\x2500\\x2500\\x2500\\x2500\\x2534\\x2500\\x2500\\x2500\\x2500\\x2534\\x2500\\x2500\\x2500\\x2500\\x2534\\x2500\\x2500\\x2500\\x2500\\x2534\\x2500\\x2500\\x2500\\x2500\\x2518\\n').
 
-display_board_row([], _).
+
+% display_board_row(+Row, +N)
+% displays a board 'Row', including the left digit 'N' that indicates the line coordinate and the pieces that are there
+display_board_row([], _N).
 display_board_row([Symbol | T], N) :-
-	(N \= -1 -> print(N); true),
-	symbol(Symbol, Value),
+	(N \= -1 -> print(N); true),  % prints the left digit
+	symbol(Symbol, Value),   % gets the symbol that corresponds to the piece that's in the current position
 	print(' \x2502\ '),
 	print(Value),
 	display_board_row(T, -1).
 
-%display all board elements
-%display_board(+GameState)
+% display_board(+GameBoard, +N)
+% displays the whole 'GameBoard'. N is the line counter, from 1 to 9 (including)
 display_board([H | []], N) :-
 	display_board_row(H, N),
 	print(' \x2502\\n'),
@@ -98,29 +174,32 @@ display_board([H | T], N) :-
 	N1 is N+1,
 	display_board(T, N1).
 
+% display_player(+Player)
+% displays the next 'Player'
+display_player(-1).
 display_player(Player) :-
 	print(Player),
 	print('\'s turn.'),
 	nl.
 
-% display_players_pieces(+Pieces)
+
+% display_players_pieces(+NPieces)
+% displays the amount of pieces that each player has in-field
 display_players_pieces(npieces(NWhitePieces, NBlackPieces)) :- 
-	print('white player has '),
+	print('White player has '),
 	print(NWhitePieces), 
 	print(' pieces.'), 
 	nl,
-	print('black player has '),
+	print('Black player has '),
 	print(NBlackPieces),
 	print(' pieces.'), 
 	nl.
 
-%displays game state
-% display_game(+GameState)
-display_game(game_state(Player, NPieces, GameBoard)) :-
+% display_game(+GameState, +Player)
+% Displays the 'GameState', as well as the current 'Player'
+display_game(game_state(_Player, NPieces, GameBoard), Player) :-
 	display_board(GameBoard, 1),
 	display_players_pieces(NPieces),
 	display_player(Player).
-
-
 
 
--- src/input.pl
+++ src/input.pl
@@ -1,23 +1,26 @@
-:-use_module(library(lists)).
-
 % get_num_from_code(+Code, -Digit, +Offset)
-% if Num is -1, Code does not mean a number
+% if Digit is -1, Code does not mean a number
 get_digit_from_code(Code, Digit, Offset) :-
     Aux is Code - Offset,
     ((Aux < 0 ; Aux > 9) -> Digit is -1; Digit is Aux).
 
-% Takes a single digit number ASCII code and converts it to a digit.
+
 % get_digit_from_num_code(+Code, -Digit)
+% Takes a single digit number ASCII code and converts it to a digit.
 get_digit_from_num_code(Code, Digit) :-
     get_digit_from_code(Code, Digit, 48).
 
+
+% get_digit_from_uppercase_letter_code(+Code, -Digit)
 % Takes an uppercase letter ASCII code and converts it to a digit.
 % Example: 65 (=:= 'A') -> 0
-% get_digit_from_uppercase_letter_code(+Code, -Digit)
 get_digit_from_uppercase_letter_code(Code, Digit) :-
     get_digit_from_code(Code, Digit, 64).
 
+
 % verify_position_input(+Line, -Position)
+% Verifies that a given 'Line' is valid position input (form A,1 -> LetterCommaNumber), unifying 'Position' with the position input
+% Else fails
 verify_position_input(Line, position(X, Y)) :-
     length(Line, 3),
 
@@ -34,7 +37,9 @@ verify_position_input(Line, position(X, Y)) :-
 
     X is TempX, Y is TempY.  % if the verifications fail, neither X nor Y are set
 
+
 % get_position_input(-Position)
+% Unifies 'Position' with valid position input
 get_position_input(Position) :-
     read_line(Line),
     (
@@ -43,14 +48,30 @@ get_position_input(Position) :-
         -> true
         ; print('That is not a valid position input. Example: \'A,1\'.'), nl, get_position_input(Position)
     ).
+
+
+% get_desired_position_input(-Position, +GameBoard, +DesiredOccupant, -Piece)
+% Asks user for a valid position that may correspond to the piece he wants to move or to the piece destination
+get_desired_position_input(position(X1, Y1), GameBoard, DesiredOccupant, Piece):-
+	get_position_input(position(XTemp, YTemp)),
+	(
+		(nth1(YTemp, GameBoard, YLine), (nth1(XTemp, YLine, dice(DesiredOccupant, _Value)); nth1(XTemp, YLine, DesiredOccupant)))
+			-> (X1 = XTemp, Y1 = YTemp, nth1(XTemp, YLine, Piece))
+			; (print('The coordinates you inserted are not \''), print(DesiredOccupant), print('\'.\n'), get_desired_position_input(position(X1, Y1), GameBoard, DesiredOccupant, Piece))
+	).
+
+
 % verify_digit_input(+Min, +Max, +Input, -Out)
+% Succeeds if 'Input' is a digit between Min and Max (including), unifying 'Out' with the digit
 verify_digit_input(Min, Max, Input, Out) :-
     length(Input, 1),
     nth0(0, Input, D),
     get_digit_from_num_code(D, Out),
     Out >= Min, Out =< Max.
 
-% get_integer_input(+Min, +Max, -Input)
+
+% get_digit_input(+Min, +Max, -Input)
+% Unifies 'Input' with a valid digit between Min and Max (including)
 get_digit_input(Min, Max, Input) :-
     read_line(Line),
     (
